95737c287ef31b5c6d31cb30820dc94572f4de3e
Dyredhead <danielyentin@gmail.com>
2023-05-30 10:14:09 -0400

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+n=$(pgrep -f "Gnome-Background-Slideshow.sh" | wc -l)
+if [ "$n" = 0]; then 
+		echo "Program is not currently running."
+		exit 0
+else
+  pkill -f "Gnome-Background-Slideshow.sh"
